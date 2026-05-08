c4b07debb8a662b645f05a15df5c33b9e597b077
danzel <savostyanov19d@mail.ru>
2026-05-08 10:46:10 +0200

    change launch settings

--- main_application/main.py
+++ main_application/main.py
@@ -17,4 +17,9 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main_app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )

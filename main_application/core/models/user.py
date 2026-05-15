b569c7cdfa1eed7000814c3e78acfb9312d52eb8
danzel <savostyanov19d@mail.ru>
2026-05-15 13:29:51 +0200

    create users table

---
+++ main_application/core/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import CheckConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    name: Mapped[str] = mapped_column(nullable=False)
+    phone: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+
+    __table_args__ = (
+        CheckConstraint(
+            "email IS NOT NULL or phone IS NOT NULL", name="email_or_phone_required"
+        ),
+    )

6ddaaa7ce6d9f470b944c948db6652c3d8eaa73f
danzel <savostyanov19d@mail.ru>
2026-05-16 16:50:56 +0200

    add create func for user

--- main_application/api/api_v1/users.py
+++ main_application/api/api_v1/users.py
@@ -4,13 +4,25 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
-from core.schemas.user import UserRead
-from crud.users import get_all_users
+from core.schemas.user import UserRead, UserCreate
+from crud import users as users_crud
 
 router = APIRouter(tags=["Users"])
 
 
 @router.get("/", response_model=List[UserRead])
 async def get_users(session: AsyncSession = Depends(db_helper.session_getter)):
-    users = await get_all_users(session=session)
+    users = await users_crud.get_all_users(session=session)
     return users
+
+
+@router.post("/", response_model=UserRead)
+async def create_user(
+    user_create: UserCreate,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    user = await users_crud.create_user(
+        user_create=user_create,
+        session=session,
+    )
+    return user

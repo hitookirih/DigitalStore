909cfe3d5e5c4f6a92416ce75d89dd3fa835af7c
danzel <savostyanov19d@mail.ru>
2026-05-20 16:28:03 +0200

    int id pkey

--- main_application/core/models/product.py
+++ main_application/core/models/product.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from .mixins.int_id_pk import IntIdPkMixin
 
 
-class Product(Base):
+class Product(IntIdPkMixin, Base):
     __tablename__ = "products"
     name: Mapped[str] = mapped_column(nullable=False)
     price: Mapped[float] = mapped_column(nullable=False)

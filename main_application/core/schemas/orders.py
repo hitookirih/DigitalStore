a3a20e86028496ec2d52d529076e1ca73ea5953d
danzel <savostyanov19d@mail.ru>
2026-05-27 14:03:29 +0200

    add schemas for orders

---
+++ main_application/core/schemas/orders.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict
+
+
+class OrderBase(BaseModel):
+    id: int
+
+
+class Order(OrderBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    user_id: int
+    product_id: int
+    quantity: int
+    total_price: float
+    created_at: datetime
+
+
+class OrderCreate(Order):
+    user_id: int
+    product_id: int
+    quantity: int = 1

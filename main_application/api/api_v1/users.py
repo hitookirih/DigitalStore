12ed471d43a2cc6923bad83697fc767e388595b5
danzel <savostyanov19d@mail.ru>
2026-06-07 11:36:51 +0200

    add user id

--- main_application/api/api_v1/users.py
+++ main_application/api/api_v1/users.py
@@ -33,6 +33,7 @@ async def create_user(
 
 @router.patch("/{user_id}/", response_model=UserRead)
 async def update_user_partial(
+    user_id: int,
     user_update: UserUpdatePartial,
     user: User = Depends(user_by_id),
     session: AsyncSession = Depends(db_helper.session_getter),
@@ -42,6 +43,7 @@ async def update_user_partial(
 
 @router.delete("/{user_id}/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(
+    user_id: int,
     user: User = Depends(user_by_id),
     session: AsyncSession = Depends(db_helper.session_getter),
 ):

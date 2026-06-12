f294c6d5e7242fb994edfa7ad56706618340d966
danzel <savostyanov19d@mail.ru>
2026-06-12 15:36:14 +0200

    create auth_user_api.py file for authenticated users

---
+++ main_application/api/api_v1/auth_user_api.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from auth.user_auth import get_current_active_auth_user
+from core.models import User, db_helper
+from core.schemas.user import UserUpdatePartial
+from crud import users as users_crud
+
+router = APIRouter(tags=["Auth_user"])
+
+
+@router.get("/me/")
+async def get_me(
+    user: User = Depends(get_current_active_auth_user),
+):
+    return {
+        "name": user.name,
+        "email": user.email,
+        "phone": user.phone,
+    }
+
+
+@router.patch("/me/")
+async def change_user_data(
+    user_update: UserUpdatePartial,
+    user: User = Depends(get_current_active_auth_user),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    return await users_crud.update_user(session, user, user_update, partial=True)
